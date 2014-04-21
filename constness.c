306247c902f28fa337b8d2861dc7e13871448051
uknowit <shashikumarhn@gmail.com>
2014-04-21 23:49:08 +0530

    Adding constness

---
+++ constness.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * http://stackoverflow.com/questions/16021454/difference-between-declared-string-and-allocated-string/16021546#16021546
+ */
+
+int main (int argc,char** argv)
+{
+	const char* constVal="ConstValue";
+	char* const constPtr=(char*)malloc(sizeof(char*));
+	char* const constPtr1="without malloc";
+	*constPtr="ConstPointer";
+	const char* val="NewValue";
+	const char* val1="NewPointer";
+	constVal=val;
+	//*constVal="Cannot do this";
+	strcpy(constPtr,val1);
+	printf("%s\n%s\n%s\n",constPtr,constVal,constPtr1);
+	free(constPtr);
+	return 0;
+}
